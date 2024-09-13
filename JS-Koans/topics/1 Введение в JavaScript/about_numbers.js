ae51e37e6a8c105572133eccbff95fa36243f1b6
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2024-09-13 15:25:20 +0500

    Update about_numbers.js

--- JS-Koans/topics/1 Введение в JavaScript/about_numbers.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_numbers.js
@@ -3,18 +3,18 @@ describe("About Numbers (about_numbers.js)", function() {
     let typeOfIntegers = typeof(6);
     let typeOfFloats = typeof(3.14159);
     // are ints and floats the same type?
-    expect(FILL_ME_IN).toBe(typeOfIntegers === typeOfFloats);
+    expect(typeOfIntegers === typeOfFloats).toBe(true); // int и float имеют один и тот же тип: "number"
     // what is the javascript numeric type?
-    expect(FILL_ME_IN).toBe(typeOfIntegers);
+    expect(typeOfIntegers).toBe("number"); // тип "number" - обозначение цифр в javascript
     // what is a integer number equivalent to 1.0?
-    expect(FILL_ME_IN).toBe(1.0);
+    expect(1).toBe(1.0); // 1 и 1.0 эквивалентны
   });
 
   it("NaN", function() {
-    let resultOfFailedOperations = 7/'apple';
+    let resultOfFailedOperations = 7 / 'apple';
     // what will satisfy the equals assertion?
-    expect(trFILL_ME_INue).toBe(isNaN(resultOfFailedOperations));
+    expect(true).toBe(isNaN(resultOfFailedOperations)); // isNaN вернет true, так как результат деления нечисловой строки
     // is NaN == NaN?
-    expect(FILL_ME_IN).toBe(resultOfFailedOperations == NaN);
+    expect(false).toBe(resultOfFailedOperations == NaN); // NaN не равен сам себе, поэтому результат будет false
   });
 });
