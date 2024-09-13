5095011fc32b1d7d4e4a20d429ca4e8e7eef2fd8
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2024-09-13 15:57:40 +0500

    Update about_strings.js

--- JS-Koans/topics/1 Введение в JavaScript/about_strings.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_strings.js
@@ -4,37 +4,37 @@ describe("About Strings (about_strings.js)", function() {
     let singleQuotedString = 'apple';
     let doubleQuotedString = "apple";
     // are the two strings equal?
-    expect(FILL_ME_IN).toBe(singleQuotedString === doubleQuotedString);
+     expect(singleQuotedString === doubleQuotedString).toBe(true);
   });
 
   it("concatenation", function() {
     let fruit = "apple";
     let dish = "pie";
     // are the two strings equal?
-    expect(FILL_ME_IN).toBe(fruit + " " + dish);
+    expect(fruit + " " + dish).toBe("apple pie");
   });
 
   it("character Type", function() {
     let characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
     // Javascript has no character type
-    expect(FILL_ME_IN).toBe(characterType);
+    expect(characterType).toBe("string");
   });
 
   it("escape character", function() {
     let stringWithAnEscapedCharacter  = "\u0041pple";
     // what  is the value of stringWithAnEscapedCharacter?
-    expect(FILL_ME_IN).toBe(stringWithAnEscapedCharacter);
+    expect(stringWithAnEscapedCharacter).toBe("Apple");
   });
 
   it("string.length", function() {
     let fruit = "apple";
     // what is the value of fruit.length?
-    expect(FILL_ME_IN).toBe(fruit.length);
+    expect(fruit.length).toBe(5);
   });
 
   it("slice", function() {
     let fruit = "apple pie";
     // what is the value of fruit.slice(0,5)?
-    expect(FILL_ME_IN).toBe(fruit.slice(0,5));
+    expect(fruit.slice(0,5)).toBe("apple");
   }); 
 });
