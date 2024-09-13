e4e173ca18fe3f8dd1587f8ef990d91509be4ea5
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2024-09-13 15:59:21 +0500

    Update about_truthyness.js

--- JS-Koans/topics/1 Введение в JavaScript/about_truthyness.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_truthyness.js
@@ -3,24 +3,24 @@ describe("About Truthyness (about_truthyness.js)", function() {
   it("truthyness of positive numbers", function() {
     let oneIsTruthy = 1 ? true : false;
     // is one truthy?
-    expect(FILL_ME_IN).toBe(oneIsTruthy);
+     expect(oneIsTruthy).toBe(true);
   });
 
   it("truthyness of negative numbers", function() {
     let negativeOneIsTruthy = -1 ? true : false;
     // is -1 truthy?
-    expect(FILL_ME_IN).toBe(negativeOneIsTruthy);
+    expect(negativeOneIsTruthy).toBe(true);
   });
 
   it("truthyness of zero", function() {
     let zeroIsTruthy = 0 ? true : false;
     // is 0 truthy?
-    expect(FILL_ME_IN).toBe(zeroIsTruthy);
+    expect(zeroIsTruthy).toBe(false);
   });
 
   it("truthyness of null", function() {
     let nullIsTruthy = null ? true : false;
     // is null truthy?
-    expect(FILL_ME_IN).toBe(nullIsTruthy);
+    expect(nullIsTruthy).toBe(false);
   });
 });
