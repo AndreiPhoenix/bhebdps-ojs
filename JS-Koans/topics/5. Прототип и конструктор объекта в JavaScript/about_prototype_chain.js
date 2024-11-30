60b3b1642d975b800a224ee7c308fedfd4dd4ddc
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2024-11-30 19:24:32 +0500

    Update about_prototype_chain.js

--- JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototype_chain.js
+++ JS-Koans/topics/5. Прототип и конструктор объекта в JavaScript/about_prototype_chain.js
@@ -24,44 +24,42 @@ describe("About Prototype Chain (about_prototype_chain.js)", function() {
    * */
   
   it("Is there an 'a' and 'b' own property on child?", function () {
-    // child.hasOwnProperty(\'a\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('a'));
-    // child.hasOwnProperty(\'b\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('b'));
+    // child.hasOwnProperty('a')?
+    expect(child.hasOwnProperty('a')).toBe(true);
+    // child.hasOwnProperty('b')?
+    expect(child.hasOwnProperty('b')).toBe(true);
   });
   
   it("Is there an 'a' and 'b' property on child?", function () {
-    // what is \'a\' value?
-    expect(FILL_ME_IN).toBe(child.a);
-    // what is \'b\' value?
-    expect(FILL_ME_IN).toBe(child.b);
+    // what is 'a' value?
+    expect(child.a).toBe(1);
+    // what is 'b' value?
+    expect(child.b).toBe(2);
   });
   
   it("If 'b' was removed, whats b value?", function () {
     delete child.b;
-    // what is \'b\' value now?
-    expect(FILL_ME_IN).toBe(child.b);
+    // what is 'b' value now?
+    expect(child.b).toBe(3); // After deletion, it should access the prototype property from father
   });
   
-  
   it("Is there a 'c' own property on child?", function () {
-    // child.hasOwnProperty(\'c\')?
-    expect(FILL_ME_IN).toBe(child.hasOwnProperty('c'));
+    // child.hasOwnProperty('c')?
+    expect(child.hasOwnProperty('c')).toBe(false);
   });
   
   // Is there a 'c' own property on child? No, check its prototype
   // Is there a 'c' own property on child.[[Prototype]]? Yes, its value is...
   it("Is there a 'c' property on child?", function () {
     // what is the value of child.c?
-    expect(FILL_ME_IN).toBe(child.c);
+    expect(child.c).toBe(4); // Accessing the property from father
   });
-  
   
   // Is there a 'd' own property on child? No, check its prototype
   // Is there a 'd' own property on child.[[Prototype]]? No, check it prototype
   // child.[[Prototype]].[[Prototype]] is null, stop searching, no property found, return...
   it("Is there an 'd' property on child?", function () {
     // what is the value of child.d?
-    expect(FILL_ME_IN).toBe(child.d);
+    expect(child.d).toBe(undefined); // 'd' does not exist in child or its prototype chain
   });
 });
